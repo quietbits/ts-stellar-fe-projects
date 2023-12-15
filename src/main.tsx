4aff5a4e368bc95d7624b197b2d8b8aee35ab0c5
Iveta <quietbits@gmail.com>
2023-12-15 13:32:08 -0500

    Add SDS

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,10 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.scss";
 
+import "@stellar/design-system/build/styles.min.css";
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
